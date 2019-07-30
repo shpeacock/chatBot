dd3b27c45078add85adb7727369fb14633213346
shpeacock <spencerpeacock@gmail.com>
2019-07-29 21:21:20 -0500

    now we chattin !

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -3,11 +3,20 @@ import { environment } from '../../environments/environment';
 
 import { ApiAiClient } from 'api-ai-javascript/es6/ApiAIClient';
 
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+export class Message {
+  constructor (public content: string, public sentBy: string) {}
+}
+
 @Injectable()
 
 export class ChatService {
     readonly token = environment.dialogflow.angularBot;
     readonly client = new ApiAiClient({accessToken: this.token});
+
+    conversation = new BehaviorSubject<Message[]>([]);
 
     constructor(){}
 
@@ -15,4 +24,21 @@ export class ChatService {
       this.client.textRequest('Who are you!')
       .then(res => console.log(res));
     }
+
+    update(msg: Message){
+      this.conversation.next([msg]);
+    }
+
+    converse(msg:string){
+      const userMessage = new Message(msg, 'user');
+      this.update(userMessage);
+
+      return this.client.textRequest(msg)
+           .then(res => {
+              const speech = res.result.fulfillment.speech;
+              const botMessage = new Message(speech, 'bot');
+              this.update(botMessage);
+            });
+    }
+
 }
